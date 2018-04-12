0e63da3ee90a00f7bbaabc057d11833553ee3c51
mmekhanov <mikhail.mekhanov@eurochem.ru>
2018-04-12 13:06:56 +0300

    Better large amount of files handling

--- nfs_clean.sh
+++ nfs_clean.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
 echo "$(date)"
-ssh suek-nfs-clu02 'rm -rfv /var/nfs/*' && echo -e '\nSUCCES remove of /var/nfs/*\n' && 
+
+ssh suek-nfs-clu02 'rm -rfv /var/nfs/*' && echo -e '\nSUCCESSFUL remove of /var/nfs/*\n' &&
+
 echo -e 'Starting new sync...\n' && cd "$(dirname "$0")" && ./nfs_sync.sh
--- nfs_sync.sh
+++ nfs_sync.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
 
-echo "$(date)"
-/usr/bin/rsync -chavz /var/nfs/ suek-nfs-clu02.moscow.eurochem.ru:/var/nfs/
+echo "starting sync at: $(date)"
+START=$(date +%s)
+
+# use rsync -chavz for small amount of files
+# use rsync -hav for large amount of files (make sure --checksum is not being used!)
+/usr/bin/rsync -hav /var/nfsshare/ uds-nfs01.moscow.eurochem.ru:/var/nfsshare/
+
+END=$(date +%s)
+DIFF=$(( ($END - $START)/60 ))
+echo "finsished sync at: $(date), it took $DIFF minutes"
+
