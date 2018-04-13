670813f149419abce366fe4110f7a1f5262d063d
mmekhanov <mikhail.mekhanov@eurochem.ru>
2018-04-13 12:36:10 +0300

    Fix legacy check command

--- nfs_check.sh
+++ nfs_check.sh
@@ -4,12 +4,12 @@
 
 source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/lib.sh"
 
-CLUSTER_IP="${CLUSTER_IP:-127.0.0.1}"
+MASTER_IP="${MASTER_IP:-127.0.0.1}"
 
-if ifconfig | grep --quiet "$CLUSTER_IP"; then
-  echo 'OK: I am MASTER node'
+if ip address list | grep --quiet "$MASTER_IP"; then
+  echo "OK: I am MASTER node (master ip $MASTER_IP)"
   exit 0
 else
-  echo 'ERROR: I am NOT MASTER node' 1>&2
+  echo "ERROR: I am NOT MASTER node (master ip $MASTER_IP)" 1>&2
   exit 1
 fi
